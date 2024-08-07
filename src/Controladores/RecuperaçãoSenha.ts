9a3a224cead5ad6b844381eabe61fd26a1f4d218
Isaac - Exon <isaac.souza@exongenomics.com>
2024-08-07 16:21:45 -0300

    ajuste de segurança recoveryCode bd

--- src/Controladores/RecuperaçãoSenha.ts
+++ src/Controladores/RecuperaçãoSenha.ts
@@ -26,6 +26,7 @@ class RedefinicaoSenha {
       const db = client.db(DB_NAME);
       const collection = db.collection("login");
 
+      // Verificar se o código de recuperação é válido
       const user = await collection.findOne({
         email,
         recoveryCode,
@@ -41,19 +42,32 @@ class RedefinicaoSenha {
 
       const hashedPassword = bcrypt.hashSync(newPassword, 10);
 
+      // Gerar um novo código de recuperação aleatório de 6 dígitos
+      const newRecoveryCode = Math.floor(
+        100000 + Math.random() * 900000
+      ).toString();
+
+      // Atualizar a senha e o código de recuperação com um novo código gerado
       await collection.updateOne(
-        { email },
+        { email, recoveryCode },
         {
           $set: {
             senha: hashedPassword,
+            recoveryCode: newRecoveryCode, // Atualiza o código de recuperação
           },
         }
       );
+
+      // Aqui você pode implementar o envio do novo código de recuperação para o e-mail do usuário, se desejar
 
       res.status(200).json({ message: "Senha redefinida com sucesso" });
     } catch (error) {
       console.error("Erro:", error);
       res.status(500).json({ error: "Erro interno do servidor" });
+    } finally {
+      if (client) {
+        client.close();
+      }
     }
   }
 }
